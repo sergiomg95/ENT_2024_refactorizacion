d8ce2c1ebc6f07a36417ba993d9c29ccd1114e96
Mañana <Maana@Aula8-04>
2024-04-30 09:01:43 +0200

    Ejercicio 5 Inicio:

---
+++ src/ejercicio5/AlgunOtraClase.java
@@ -0,0 +1,10 @@
+package ejercicio5;
+
+public class AlgunOtraClase {
+
+	public void setDireccion(Cliente cliente, String nuevaDireccion) {
+		cliente.setDireccion(nuevaDireccion);
+		// Otro código aquí
+	}
+
+}
---
+++ src/ejercicio5/Cliente.java
@@ -0,0 +1,34 @@
+package ejercicio5;
+
+public class Cliente {
+
+	    private String nombre;
+	    private String apellido;
+	    private String direccion;
+	    private String ciudad;
+	    private String codigoPostal;
+
+	    // Getters y setters
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public void setApellido(String apellido) {
+	        this.apellido = apellido;
+	    }
+
+	    public void setDireccion(String direccion) {
+	        this.direccion = direccion;
+	    }
+
+	    public void setCiudad(String ciudad) {
+	        this.ciudad = ciudad;
+	    }
+
+	    public void setCodigoPostal(String codigoPostal) {
+	        this.codigoPostal = codigoPostal;
+	    }
+	
+
+}
